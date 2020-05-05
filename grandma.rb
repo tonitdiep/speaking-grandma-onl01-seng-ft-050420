f59aec08c6a2446746e6e7d6d2d3ed9551d2e482
Toni  Diep <adiep123@gmail.com>
2020-05-05 15:31:37 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -17,13 +17,13 @@
 
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-     return "I LOVE YOU TOO PUMPKIN!"
+      "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-     return "NO, NOT SINCE 1938!"
+      "NO, NOT SINCE 1938!"
   elsif phrase = phrase.upcase
-      return "HUH?! SPEAK UP, SONNY!"
+       "HUH?! SPEAK UP, SONNY!"
   else
-     return "NO, NOT SINCE 1938!"
+      "NO, NOT SINCE 1938!"
   end
 end
 
